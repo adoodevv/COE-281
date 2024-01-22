8ade7c5fb1130ac3f064352643dd4c14dd81293f
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-22 08:38:48 +0000

    apostrope lesson

---
+++ Python/apostrophe.py
@@ -0,0 +1,7 @@
+message = "One of Python's strenghts is a diverse community."
+print(message)
+#The apostrophe appears inside a set of double quotes, so the Python interpreter has no trouble reading the string correctly
+
+message = 'One of Python's strengths is its diverse community.'
+print(message)
+#However, if you use single quotes, Python can’t identify where the string should end
