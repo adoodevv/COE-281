2340591e1e81c8f6a212e7b4d159ecc0377c4dc8
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-23 22:06:46 +0000

    more list operations

--- Python/motorcycles.py
+++ Python/motorcycles.py
@@ -1,5 +1,29 @@
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
-motorcycles[0] = 'ducati'
+#modifying items in a list
+motorcycles[0] = 'ducati' #modifying first item
 print(motorcycles)
+
+#adding elements to a list
+motorcycles.append('honda')
+print(motorcycles)
+
+#inserting elements into list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'red bull')
+print(motorcycles)
+
+#removing items from a list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
+
+#removing items using the pop() method
+#this method allows you to work with the removed item after removing it
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
