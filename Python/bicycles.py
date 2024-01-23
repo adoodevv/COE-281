503719cd791a24721eedfea2bb6f2f20a207fea3
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-23 21:32:13 +0000

    accessing list items

--- Python/bicycles.py
+++ Python/bicycles.py
@@ -1,2 +1,12 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+#index starts at 0
+
+print(bicycles[0]) #accessing first item in the list
+print(bicycles[0].title())
+
+print(bicycles[-1]) #accessing last item in the list
+
+message = f"My first bicycle was a {bicycles[0].title()}" #using individual values from a list
+print(message)
