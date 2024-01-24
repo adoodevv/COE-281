ee16da8e10d279a476d71bdf7d277760d816be45
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-24 13:37:15 +0000

    generate square numbers shorter approach

--- Python/square_numbers.py
+++ Python/square_numbers.py
@@ -4,3 +4,10 @@ for value in range(1, 11):
    squares.append(square)
 
 print(squares)
+
+#shorter approach
+squares = []
+for value in range(1, 11):
+   squares.append(value ** 2)
+
+print(squares)
