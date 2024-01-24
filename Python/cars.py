df9a757783d9816be16a384b13bbd1d90dfd2997
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-24 13:01:40 +0000

    reverse sort

--- Python/cars.py
+++ Python/cars.py
@@ -1,4 +1,8 @@
 #sort list permanently
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-cars.sort()
+cars.sort() #alphabetical order
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True) #reverse alphabetical order
 print(cars)
