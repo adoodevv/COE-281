6883cefe99f5afe07606b8b6640102ac944e0ea0
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-24 13:05:09 +0000

    length of lists

--- Python/cars.py
+++ Python/cars.py
@@ -6,3 +6,7 @@ print(cars)
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort(reverse=True) #reverse alphabetical order
 print(cars)
+
+#length of list with len function
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
