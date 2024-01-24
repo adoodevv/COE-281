1ff11901856ba7f0481c3e2b3b0e4d170c882c00
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-24 13:15:56 +0000

    doing more with loops

--- Python/magicians.py
+++ Python/magicians.py
@@ -2,3 +2,8 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
    print(magician)
+
+magicians = ['haqq', 'esther', 'joe']
+for magician in magicians:
+   print(f"{magician.title()}, that was a great trick!")
+print("Thank you, everyone. That was a great magic show")
