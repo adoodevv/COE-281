d0f7e16a3b88f456c51a316a1c89ed471082c63d
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-29 02:22:49 +0000

    using lists with loops

--- Python/16-toppings.py
+++ Python/16-toppings.py
@@ -8,3 +8,9 @@ if 'mushrooms' in requested_toppings:
    print("Adding mushrooms.")
 if 'pepperoni' in requested_toppings:
    print("Adding pepperoni.")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+   print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
