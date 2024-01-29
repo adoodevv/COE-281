d9dbde5563f60b9d0deb9a8fb5d772db92641e55
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-29 21:33:14 +0000

    add floating point numbers for meaningful message

--- C/2-temperature.c
+++ C/2-temperature.c
@@ -4,7 +4,7 @@
    for fahr = 0, 20, ..., 300 */
 
 int main() {
-   int fahr, celcius;
+   float fahr, celcius;
    int lower, upper, step;
 
    lower = 0;
@@ -13,8 +13,8 @@ int main() {
 
    fahr = lower;
    while (fahr <= upper) {
-      celcius = 5 * (fahr - 32) / 9;
-      printf("%d\t%d\n", fahr, celcius);
+      celcius = (5.0/ 9.0) * (fahr - 32.0);
+      printf("%3.0f\t%6.1f\n", fahr, celcius);
       fahr += step;
    }
 }
