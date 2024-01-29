15e173fe6864b7e8e47c96f3a867d602dbf731a3
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-29 02:17:44 +0000

    testing multiple conditions

--- Python/16-toppings.py
+++ Python/16-toppings.py
@@ -2,3 +2,9 @@ requested_toppings = 'mushrooms'
 
 if requested_toppings != 'anchovies': # inequality check
    print("Hold the anchovies")
+
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+   print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+   print("Adding pepperoni.")
