7f9bd90770bd29388d8cefa9fb0caf8acb847b29
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-31 22:54:42 +0000

    changing values in dictionary

--- Python/06/1-alien.py
+++ Python/06/1-alien.py
@@ -20,3 +20,18 @@ print(f"The alien is {alien_0['color']}.")
 
 alien_0['color'] = 'yellow'
 print(f"The alien is now {alien_0['color']}.")
+
+# determine alien position based on speed
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'slow'}
+print(f"Original position: {alien_0['x_position']}")
+
+if alien_0['speed'] == 'slow':
+   x_increment = 1
+elif alien_0['speed'] == 'medium':
+   x_increment = 2
+else:
+   x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New positon: {alien_0['x_position']}")
