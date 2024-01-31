76f0ae17b48fe818309470161079eed3720c94f4
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-31 17:57:09 +0000

    chapter 1

---
+++ C/01/5-character_counting.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* count characters in input */
+int main () {
+   long nc;
+   nc = 0;
+   while (getchar() != EOF) {
+      ++nc; /* increment by one */
+   printf("%1d\n", nc);
+   }
+}
