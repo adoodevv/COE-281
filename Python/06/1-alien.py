e95bc366f11edbe2327d0bb2748a77a446dde12b
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-31 17:53:33 +0000

    modify values in dictionary

--- Python/06/1-alien.py
+++ Python/06/1-alien.py
@@ -13,3 +13,10 @@ print(alien_0)
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
+
+# modifying values in dictionary
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
