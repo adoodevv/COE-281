7d7f98f0a4a26b67cf801a6c314f3ddd81d9909e
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-31 17:49:46 +0000

    accessing values and adding key-value pairs

--- Python/06/1-alien.py
+++ Python/06/1-alien.py
@@ -1,4 +1,15 @@
 alien_0 = {'color': 'green', 'points': 5}
 
-print(alien_0['color'])
+print(alien_0['color']) # reading data from dictionary
 print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+
+# adding new key-value pairs
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
