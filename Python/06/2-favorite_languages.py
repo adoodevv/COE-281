a695798becb6b1a17a82dae5d9011e6107d32d79
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-02 16:14:22 +0000

    looping through keys in a dictionary

--- Python/06/2-favorite_languages.py
+++ Python/06/2-favorite_languages.py
@@ -11,3 +11,7 @@ print(f"Sarah's favorite language is {language}.")
 #looping through dictionary
 for name, language in favorite_languages.items():
    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+#looping through keys in a dictionary
+for name in favorite_languages.keys():
+   print(name.title())
