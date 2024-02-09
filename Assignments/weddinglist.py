ade37ceecf9c2c298f92495492c6a34c54446e6b
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-09 09:07:56 +0000

    add functinality to print in lower

--- Assignments/weddinglist.py
+++ Assignments/weddinglist.py
@@ -1,11 +1,19 @@
 emails = ['joe@outlook.com', 'me@gmail.com', 'kofi@gmail.com']
 print(emails)
+
 emails.sort()
 print(emails)
-emails.append('mummy@gmail.com')
+
+emails.append('Mummy@gmail.com')
 print(emails)
+
 del emails[2]
 print(emails)
+
 print(len(emails))
+
 emails.sort()
 print(emails)
+
+for mail in emails:
+   print(mail.lower())
