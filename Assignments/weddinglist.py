d5719978323549b51caafa53b2f4b5dc5e161a50
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-09 08:53:54 +0000

    list test in lab

---
+++ Assignments/weddinglist.py
@@ -0,0 +1,11 @@
+emails = ['joe@outlook.com', 'me@gmail.com', 'kofi@gmail.com']
+print(emails)
+emails.sort()
+print(emails)
+emails.append('mummy@gmail.com')
+print(emails)
+del emails[2]
+print(emails)
+print(len(emails))
+emails.sort()
+print(emails)
