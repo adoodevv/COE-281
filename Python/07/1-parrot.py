9c66e80812f7cdcc06a6501c8df39a4663b34979
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-10 14:40:59 +0000

    use flags in while loops

--- Python/07/1-parrot.py
+++ Python/07/1-parrot.py
@@ -5,8 +5,13 @@ print(message)
 # using while loops
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
-message = ""
 
-while message != 'quit':
- message = input(prompt)
- print(message)
+# adding a flag(a signal to end the program)
+active = True
+while active:
+   message = input(prompt)
+
+   if message == 'quit':
+      active = False
+   else:
+      print(message)
