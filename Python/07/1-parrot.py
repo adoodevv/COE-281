d18908113126be21914dfce0d8919e3ed444d6c8
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-10 14:28:17 +0000

    use while loop to break input

--- Python/07/1-parrot.py
+++ Python/07/1-parrot.py
@@ -1,3 +1,12 @@
 # the input funtion
 message = input("Tell me something, and I will repeat it back to you: ")
 print(message)
+
+# using while loops
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+
+while message != 'quit':
+ message = input(prompt)
+ print(message)
