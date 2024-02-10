861a3e6387563b74be6da2e8ef88c9f176abd2f5
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-10 13:39:29 +0000

    form multi-line strings with user input

---
+++ Python/07/2-greeter.py
@@ -0,0 +1,7 @@
+# forming a multi-line string
+prompt = "If you share your name, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+
+name = input(prompt)
+print(f"\nHello, {name}!")
