9729701ae9e06d64e25a01de57cabcd4aea759e9
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-11 09:15:19 +0000

    copy items from one list to another

---
+++ Python/07/7-confirmed_users.py
@@ -0,0 +1,12 @@
+unconfirmed_users = ['adoo', 'darko', 'jonathan']
+confirmed_users = []
+
+while unconfirmed_users:
+   current_user = unconfirmed_users.pop()
+   print(f"Verifying user: {current_user.title()}")
+
+   confirmed_users.append(current_user)
+
+print("\nThe following users have been confirmed: ")
+for user in confirmed_users:
+   print(user.title())
