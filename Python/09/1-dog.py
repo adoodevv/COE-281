0c32c3df69def6f8958025a514c69cf3a78b28e1
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-18 13:09:08 +0000

    first attempt at classes

---
+++ Python/09/1-dog.py
@@ -0,0 +1,22 @@
+""" A simple attempt to model a dog """
+class Dog:
+   def __init__(self, name, age):
+       self.name = name # attribute
+       self.age = age
+   
+   def sit(self):
+         print(f"{self.name} is now sitting")
+
+   def roll_over(self):
+         print(f"{self.name} rolled over!")
+
+my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
+
+print(f"My dog's name is {my_dog.name}.") # accessing the attribute
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
