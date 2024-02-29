5e1cd9aad46fa2ca95ca46ffb1de03cf3ace68d9
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-02-29 03:52:05 +0000

    add comments for clarity

--- Python/10/1-file_reader.py
+++ Python/10/1-file_reader.py
@@ -1,7 +1,7 @@
-from pathlib import Path
+from pathlib import Path # Import the Path class from the pathlib module.
 
-path = Path('Python/10/pi_digits.txt')
-contents = path.read_text()
-lines = contents.splitlines()
+path = Path('Python/10/pi_digits.txt') # Create a Path object that points to the file you want to read.
+contents = path.read_text() # Read the contents of the file and store them as a string in contents.
+lines = contents.splitlines() # Split the contents of the file on the newline character to get a list of lines from the file.
 for line in lines:
    print(line)
