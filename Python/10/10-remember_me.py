45fcc438bd14beed2ef04b0ccb6401bfc75c541a
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-02 13:59:43 +0000

    code rafactoring

--- Python/10/10-remember_me.py
+++ Python/10/10-remember_me.py
@@ -1,10 +1,29 @@
 from pathlib import Path
 import json
 
-username = input("What is your name? ")
+def get_stored_username():
+   path = Path('Python/10/username.json')
+   try:
+      content = path.read_text()
+      username = json.loads(content)
+   except FileNotFoundError:
+      return None
+   else:
+      return username
+   
+def get_new_username():
+   path = Path('Python/10/username.json')
+   username = input("What is your name? ")
+   path.write_text(json.dumps(username))
+   return username
 
-path = Path('Python/10/username.json')
-content = json.dumps(username)
-path.write_text(content)
+def greet_user():
+   path = Path('Python/10/username.json')
+   username = get_stored_username()
+   if username:
+      print(f"Welcome back {username}!")
+   else:
+      username = get_new_username()
+      print(f"We'll remember you when you come back, {username}!")
 
-print(f"We'll remember you when you come back, {username}!")
+greet_user()
