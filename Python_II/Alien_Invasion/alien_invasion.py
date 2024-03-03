f0b3e18e51594e2266aa400be8dbd51e4c3a057f
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-03 20:13:51 +0000

    create settings and import successfully

--- Python_II/Alien_Invasion/alien_invasion.py
+++ Python_II/Alien_Invasion/alien_invasion.py
@@ -2,15 +2,18 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
    def __init__(self):
       pygame.init()
       self.clock = pygame.time.Clock()
+      self.settings = Settings()
 
-      self.screen = pygame.display.set_mode((1200, 800))
+      self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
       pygame.display.set_caption("Alien Invasion")
 
-      self.bg_color = (230, 230, 230)
+      self.bg_color = (self.settings.bg_color)
 
    def run_game(self):
       while True:
@@ -20,7 +23,7 @@ class AlienInvasion:
 
          self.screen.fill(self.bg_color)
 
-         pygame.event.flip()
+         pygame.display.flip()
          self.clock.tick(60)
 
 if __name__ == '__main__':
