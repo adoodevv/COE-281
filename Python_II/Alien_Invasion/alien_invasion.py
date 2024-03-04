c09e14634283ff680172644064f323679a16c7db
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-04 22:46:53 +0000

    create bullets

--- Python_II/Alien_Invasion/alien_invasion.py
+++ Python_II/Alien_Invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
    def __init__(self):
@@ -20,12 +21,19 @@ class AlienInvasion:
       pygame.display.set_caption("Alien Invasion")
 
       self.ship = Ship(self)
+      self.bullets = pygame.sprite.Group()
       self.bg_color = (self.settings.bg_color)
 
    def run_game(self):
       while True:
          self._check_events()
          self.ship.update()
+         self.bullets.update()
+         for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+               self.bullets.remove(bullet)
+         print(len(self.bullets))
+
          self._update_screen()
          self.clock.tick(60)
 
@@ -45,14 +53,24 @@ class AlienInvasion:
          self.ship.moving_left = True
       elif event.key == pygame.K_q:
          sys.exit()
+      elif event.key == pygame.K_SPACE:
+         self._fire_bullet()
 
    def _check_keyup_events(self, event):
       if event.key == pygame.K_RIGHT:
          self.ship.moving_right = False
       elif event.key == pygame.K_LEFT:
          self.ship.moving_left = False
+
+   def _fire_bullet(self):
+      if len(self.bullets) < self.settings.bullets_allowed:
+         new_bullet = Bullet(self)
+         self.bullets.add(new_bullet)
+
    def _update_screen(self):
       self.screen.fill(self.bg_color)
+      for bullet in self.bullets.sprites():
+         bullet.draw_bullet()
       self.ship.blitme()
 
       pygame.display.flip()
