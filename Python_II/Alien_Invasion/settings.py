7254a2c723dde2d0922f85eaef5d1e982c118911
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-04 22:17:03 +0000

    add quit, left and right edge border restrictions and ship speed functionality

--- Python_II/Alien_Invasion/settings.py
+++ Python_II/Alien_Invasion/settings.py
@@ -5,3 +5,8 @@ class Settings:
          self.screen_width = 1100
          self.screen_height = 700
          self.bg_color = (230, 230, 230)
+
+         # Ship settings
+         self.ship_speed = 2.5
+
+          # Bullet settings
--- Python_II/Alien_Invasion/ship.py
+++ Python_II/Alien_Invasion/ship.py
@@ -4,6 +4,7 @@ class Ship:
    def __init__(self, ai_game):
       """Initialize the ship and set its starting position."""
       self.screen = ai_game.screen
+      self.settings = ai_game.settings
       self.screen_rect = ai_game.screen.get_rect()
       # Load the ship image and get its rect.
       self.image = pygame.image.load('Python_II\Alien_Invasion\images\ship.bmp')
@@ -11,14 +12,18 @@ class Ship:
       # Start each new ship at the bottom center of the screen.
       self.rect.midbottom = self.screen_rect.midbottom
 
+      self.x = float(self.rect.x)
+
       self.moving_right = False
       self.moving_left = False
 
    def update(self):
-      if self.moving_right:
-         self.rect.x += 1
-      elif self.moving_left:
-         self.rect.x -= 1
+      if self.moving_right and self.rect.right < self.screen_rect.right:
+         self.x += self.settings.ship_speed
+      elif self.moving_left and self.rect.left > 0:
+         self.x -= self.settings.ship_speed
+
+      self.rect.x = self.x
 
    def blitme(self):
       """Draw the ship at its current location."""
