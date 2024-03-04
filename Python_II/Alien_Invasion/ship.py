e68cd5c415ce0e841cb7cd236839648be41b3dc4
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-04 21:11:50 +0000

    add left and right ship movements

--- Python_II/Alien_Invasion/ship.py
+++ Python_II/Alien_Invasion/ship.py
@@ -11,6 +11,15 @@ class Ship:
       # Start each new ship at the bottom center of the screen.
       self.rect.midbottom = self.screen_rect.midbottom
 
+      self.moving_right = False
+      self.moving_left = False
+
+   def update(self):
+      if self.moving_right:
+         self.rect.x += 1
+      elif self.moving_left:
+         self.rect.x -= 1
+
    def blitme(self):
       """Draw the ship at its current location."""
       self.screen.blit(self.image, self.rect)
