736e83b3498c48c0f61c77fdf911e67239698101
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-05 07:03:27 +0000

    add collision to detect shooting

--- Python_II/Alien_Invasion/alien_invasion.py
+++ Python_II/Alien_Invasion/alien_invasion.py
@@ -119,6 +119,9 @@ class AlienInvasion:
          if bullet.rect.bottom <= 0:
             self.bullets.remove(bullet)
 
+      collisions = pygame.sprite.groupcollide(
+         self.bullets, self.aliens, True, True)
+
 if __name__ == '__main__':
    ai = AlienInvasion()
    ai.run_game()
